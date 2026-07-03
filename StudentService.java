1402ad35c9fa04d174595c2c03bfdf5b99a7f081
Shirishaponde <pondeshirisha@gmail.com>
2026-07-03 15:16:50 +0530

    Add ArrayList CRUD operations and validation

---
+++ StudentService.java
@@ -0,0 +1,91 @@
+import java.util.ArrayList;
+
+public class StudentService {
+
+    ArrayList<Student> students = new ArrayList<>();
+
+    // public void addStudent(Student student) {
+    // students.add(student);
+    // System.out.println("Student Added Successfully");
+    // }
+    public void addStudent(Student student) {
+
+        for (Student s : students) {
+
+            if (s.getId() == student.getId()) {
+
+                System.out.println("Student ID Already Exists");
+
+                return;
+            }
+        }
+
+        students.add(student);
+
+        System.out.println("Student Added Successfully");
+    }
+
+    public void displayStudents() {
+
+        if (students.isEmpty()) {
+            System.out.println("No Records Found");
+            return;
+        }
+
+        for (Student s : students) {
+            s.display();
+            System.out.println("----------------");
+        }
+    }
+
+    public void searchStudent(int id) {
+
+        for (Student s : students) {
+            if (s.getId() == id) {
+                s.display();
+                return;
+            }
+        }
+
+        System.out.println("Student Not Found");
+    }
+
+    public void updateStudent(int id, String name, String course) {
+
+        for (Student s : students) {
+            if (s.getId() == id) {
+                s.setName(name);
+                s.setCourse(course);
+                System.out.println("Updated Successfully");
+                return;
+            }
+        }
+
+        System.out.println("Student Not Found");
+    }
+
+    public void deleteStudent(int id) {
+
+        for (Student s : students) {
+
+            if (s.getId() == id) {
+
+                students.remove(s);
+
+                System.out.println("Student Deleted Successfully");
+
+                return;
+            }
+        }
+
+        System.out.println("Student Not Found");
+
+    }
+
+    public void countStudents() {
+
+        System.out.println(
+                "Total Students: " + students.size());
+
+    }
+}
